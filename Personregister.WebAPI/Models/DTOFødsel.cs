d89d621deb063a1897444e3850057c3063efeefc
sophiesylta <sophie.sylta@gmail.com>
2022-09-08 14:52:12 +0200

    Lagt til DTO for postFødsel

---
+++ Personregister.WebAPI/Models/DTOFødsel.cs
@@ -0,0 +1,13 @@
+﻿using Personregister.Domene;
+
+namespace Personregister.WebAPI.Models
+{
+    public class DTOFødsel
+    {
+        public long personnummerMor { get; set; }
+        public long personnummerFar { get; set; }
+        public Person barn { get; set; }
+        public DateTime fødselTid { get; set; }
+
+    }
+}
