b82190c8050e9bc6ed12cb66f9aec3ce0fb967fe
sophiesylta <sophie.sylta@gmail.com>
2022-09-12 09:31:52 +0200

    DødsfallService

---
+++ Personregister.Application/Contracts/IDødsfallService.cs
@@ -0,0 +1,10 @@
+﻿using Personregister.Domene;
+
+
+namespace Personregister.Application.Contracts
+{
+    public interface IDødsfallService
+    {
+        public Dødsfall add(Dødsfall dødsfall);
+    }
+}
---
+++ Personregister.Application/DødsfallService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using Personregister.Application.Contracts;
+using Personregister.Application.Contracts.Repository;
+using Personregister.Domene;
+
+namespace Personregister.Application
+{
+    public class DødsfallService : IDødsfallService
+    {
+        private readonly IDødsfallRepository dødsfallRepository;
+        private readonly INavnService navnService;
+        private readonly IPersonRepository personRepository;
+        private readonly ILogger<DødsfallService> logger;
+
+        public DødsfallService(IDødsfallRepository dødsfallRepository, INavnService navnService, IPersonRepository personRepository, ILogger<DødsfallService> logger)
+        {
+            this.dødsfallRepository = dødsfallRepository;
+            this.navnService = navnService;
+            this.personRepository = personRepository;
+            this.logger = logger;
+        }
+
+        public Dødsfall add(Dødsfall dødsfall)
+        {
+            //sjekk om person eksisterer, i så fall bruk denne, ellers throw exception
+            var person = personRepository.getPerson(dødsfall.person.Personnummer);
+
+            if (person == null)
+            {
+                logger.LogError($"Finner ikke person med personnummer{dødsfall.person.Personnummer}");
+                throw new Exception($"Finner ikke person med personnummer{dødsfall.person.Personnummer}");
+            }
+            dødsfall.person = person;
+
+            //sjekk om dødsfall eksisterer, i så fall, returneres dette, ellers opprett nytt
+            var d = dødsfallRepository.getDødsfall(dødsfall.person.Personnummer);
+
+            if (d != null) 
+            {
+                logger.LogError("Dødsfall allerede registrert");
+                return dødsfall;
+            }
+
+            dødsfallRepository.add(dødsfall);
+            return dødsfall;
+          
+        }
+    }   
+}
